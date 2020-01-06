1b94c0dcccb785baf2a0db184c10618d7f78d9c0
VJMatheW <mail2vjmathew@gmail.com>
2020-01-06 15:15:54 +0530

    Aurora billing_app  API end point

--- Server/Routes/aurora.js
+++ Server/Routes/aurora.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
 router.post('/billing_app',async (req, res)=>{
-    console.log(req.body)
+    /**
+        body = {
+            from : 'billme',
+            shop : 'shop name',
+            content : 'invoice and invoice_items data'
+        }
+     */
+    let data = req.body;
+    data.content = JSON.parse(decodeURIComponent(req.body.content))
+    console.log(data)
 
     res.status(200).json({
         hostname : 'colorhomes.ddns.net',
